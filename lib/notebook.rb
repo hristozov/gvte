89f8759700264fc3a1dfc4a2aa3f004a0a2e70d3
Georgi Hristozov <georgi@forkbomb.nl>
2011-10-29 14:39:37 +0300

    If the output changes in a terminal, the corresponding tab label will be bolded.

--- lib/notebook.rb
+++ lib/notebook.rb
@@ -3,19 +3,22 @@ module Gvte
     def initialize options
       super()
       @options = options
+      @bolded_tabs = {}
       signal_connect("page-added", &toggle_tab_bar)
       signal_connect("page-removed", &toggle_tab_bar)
+      signal_connect("switch-page", &handle_switch_page)
     end
 
     def add_shell(move_focus=true)
       term = Vte::Terminal.new().show_all
-      term.signal_connect("window-title-changed", &update_tab_title(term))
+      term.signal_connect("window-title-changed", &update_label(term))
       term.signal_connect("eof", &handle_eof(term))
+      term.signal_connect("contents-changed", &handle_contents_changed(term))
       pid = term.fork_command(@options[:sh], nil, nil, @options[:dir])
 
       append_page term
       self.page = page_num term if move_focus
-      set_tab_label_text(term, @options[:sh])
+      set_tab_label_text term, @options[:sh]
 
       $stderr.puts "Spawned shell (#{pid}) with options #{@options}"
     end
@@ -24,15 +27,41 @@ module Gvte
       remove_page page
     end
 
-    def update_tab_title term
+    def update_label term
       Proc.new do
-        set_tab_label_text(term,term.window_title)
+        title = term.window_title
+        if title != nil
+        if @bolded_tabs[term]
+          (get_tab_label term).set_markup "<b>#{title}</b>"
+        else
+          set_tab_label_text term, title
+        end
+        end
       end
     end
 
     def handle_eof term
       Proc.new do
         remove_page(page_num term)
+      end
+    end
+
+    def handle_contents_changed term
+      Proc.new do
+        if term != current_term
+          @bolded_tabs[term] = true
+          update_label term
+        end
+      end
+    end
+
+    def handle_switch_page
+      Proc.new do
+        term = current_term
+        @bolded_tabs[term] = nil
+        #update_label(current_term).call()
+        pro = update_label(term)
+        pro.call()
       end
     end
 
