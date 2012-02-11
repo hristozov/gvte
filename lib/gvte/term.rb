b466658bc7c6ec1a1cb9068e378548c4dea6250d
Georgi Hristozov <georgi@forkbomb.nl>
2012-02-11 22:24:16 +0200

    (minor) Fixing a forgotten signal handler in the Term.

--- lib/gvte/term.rb
+++ lib/gvte/term.rb
@@ -4,10 +4,6 @@ module Gvte
       super()
       @config = config
 
-      signal_connect("eof") {
-        signal_emit("destroy")
-      }
-
       pid = self.fork_command(@config.sh, nil, nil, @config.dir)
       $stderr.puts "Spawned shell (#{pid}) with options #{@config}"
     end
